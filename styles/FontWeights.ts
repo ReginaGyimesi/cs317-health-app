94a0a208ae25e6616d8e19d02e01dcc19486ddbb
Regina Gyimesi CS2019 <regina.gyimesi.2019@uni.strath.ac.uk>
2022-02-25 14:26:33 +0100

    feat: add styles config

---
+++ styles/FontWeights.ts
@@ -0,0 +1,9 @@
+export const FontWeights = {
+  Roboto300: "Roboto-Light",
+  Roboto400: "Roboto-Regular",
+  Roboto500: "Roboto-Medium",
+  Roboto600: "Roboto-SemiBold",
+  Roboto700: "Roboto-Bold",
+  Roboto800: "Roboto-ExtraBold",
+  Roboto900: "Roboto-Black",
+};
---
+++ styles/index.ts
@@ -0,0 +1,7 @@
+import { Colors } from "./Colors";
+import { FontSizes } from "./FontSizes";
+import { FontVariants } from "./FontVariants";
+import { FontWeights } from "./FontWeights";
+import { LineHeights } from "./LineHeights";
+
+export { Colors, FontSizes, FontVariants, LineHeights, FontWeights };
