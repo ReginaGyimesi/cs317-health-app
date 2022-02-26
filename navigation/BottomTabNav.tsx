bfc0c1148022d6ee344abe4881383f42eea1d3f7
Eamonn McClay CS2019 <eamonn.mcclay.2019@uni.strath.ac.uk>
2022-02-26 01:51:34 +0000

    Added styling to navigation bar

--- navigation/BottomTabNav.tsx
+++ navigation/BottomTabNav.tsx
@@ -4,6 +4,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "../views/home/HomeScreen";
 import { SoundScreen } from "../views/sound/SoundScreen";
 import { TrackerScreen } from "../views/tracker/TrackerScreen";
+import { SettingsScreen } from "../views/settings/SettingsScreen";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,12 +15,47 @@ export const Navigator = () => {
       <Tab.Navigator
         screenOptions={{
           tabBarShowLabel: true,
+          tabBarStyle: {
+            backgroundColor: '#0e142f'
+          }
         }}
       >
-        <Tab.Screen name="home" component={HomeScreen} />
-        <Tab.Screen name="sounds" component={SoundScreen} />
-        <Tab.Screen name="tracker" component={TrackerScreen} />
-        <Tab.Screen name="settings" component={HomeScreen} />
+        <Tab.Screen
+          name="home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size}/>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="sounds"
+          component={SoundScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="music" color={color} size={size}/>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="tracker"
+          component={TrackerScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="clock" color={color} size={size}/>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="settings"
+          component={SettingsScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="cog" color={color} size={size}/>
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
