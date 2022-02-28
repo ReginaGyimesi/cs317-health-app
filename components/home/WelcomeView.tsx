2251e58599108674f0d74685fda6418a4d617b01
Regina Gyimesi CS2019 <regina.gyimesi.2019@uni.strath.ac.uk>
2022-02-28 14:36:53 +0100

    feat: img and text changes based on local time

--- components/home/WelcomeView.tsx
+++ components/home/WelcomeView.tsx
@@ -1,17 +1,28 @@
 import { StyleSheet, View, Text, ImageBackground } from "react-native";
 import { Colors, FontVariants } from "../../styles";
 
+let currtime = new Date().getHours();
+const nighttime = 5 >= currtime && currtime >= 18;
 export const WelcomeView = () => {
+  const morningtime = 5 < currtime && currtime < 12;
+  const afternoontime = 12 <= currtime && currtime < 18;
   return (
     <View>
       <ImageBackground
         imageStyle={{ borderBottomLeftRadius: 20, borderBottomRightRadius: 20 }}
-        source={require("../../assets/images/bgdarkmoon.png")}
+        source={
+          nighttime
+            ? require("../../assets/images/bgdarkmoon.png")
+            : require("../../assets/images/bglight.png")
+        }
         style={styles.imgcontainer}
       >
         <View style={styles.textwrapper}>
           <Text style={[FontVariants.introThin, styles.text]}>
-            Good evening,
+            {morningtime && "Good morning,"}
+            {afternoontime && "Good afternoon,"}
+
+            {nighttime && "Good evening,"}
           </Text>
           <Text
             style={[FontVariants.introBold, styles.text, { paddingTop: 8 }]}
@@ -30,7 +41,7 @@ const styles = StyleSheet.create({
     marginTop: 120,
   },
   text: {
-    color: Colors.grey20,
+    color: nighttime ? Colors.grey20 : Colors.grey60,
   },
   imgcontainer: {
     height: 224,
